88f1c2670120ebc8f6b33d04ff045ebbaf7e26ad
romeo <romeomeshack34@gmail.com>
2023-05-04 02:14:26 +0100

    Server entry point

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
+import connect from "./db.config";
+import envConfig from "./env.config";
 import ExpressConfig from "./express.config";
+import routes from "./routes";
 
-const app = ExpressConfig()
-const port = process.env.port || 500;
+const app = ExpressConfig();
+const { PORT } = envConfig();
 
-app.listen(port, () => console.log("App is serving on port: ", port));
+app.listen(PORT, () => {
+  console.log("App is serving on port: ", PORT);
+
+  // DB connection
+  connect();
+
+  // Url routes
+  routes(app);
+});
