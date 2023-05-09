1ad6af45feb45231d5993db2f46bbc17fe61346e
romeo <romeomeshack34@gmail.com>
2023-05-09 23:03:46 +0100

    Removed controller function declaration to 'controllers' directory.

--- src/routes.ts
+++ src/routes.ts
@@ -1,53 +1,18 @@
-import { Application, Request, Response } from "express";
-import { nanoid } from "nanoid";
-import urlModel from "./models/shortenUrl.model";
+import { Application } from "express";
+
+// Controllers
+import {
+  indexController,
+  shortenUrlController,
+  redirectShortenedUrlController,
+} from "./controllers/url.controller";
 
 export default function routes(app: Application) {
-  app.get("/", (req: Request, res: Response) => {
-    res
-      .status(200)
-      .send("Welcome to the yet another but spectacular URL shortening SaaS");
-  });
+  app.get("/", indexController);
 
   // Shorten Url
-  app.post("/shorten", async (req: Request, res: Response) => {
-    const originalUrl = req.body.originalUrl;
-    const shortenUrl = nanoid();
-
-    const url = new urlModel({ originalUrl, shortenUrl });
-
-    try {
-      // Save to DB
-      url.save();
-      res.status(202).json({ originalUrl, shortenUrl });
-    } catch (e) {
-      console.log(e);
-
-      res.status(500).json({ message: "internal server error" });
-    }
-  });
+  app.post("/marlink/shortenurl", shortenUrlController);
 
   // Redirect to original URl
-  app.get("/:shortenUrl", async (req: Request, res: Response) => {
-    const shortenUrl = req.params.shortenUrl;
-
-    try {
-      const url = await urlModel.findOne({ shortenUrl });
-
-      if (!url) {
-        res
-          .status(404)
-          .json({
-            statusCode: 404,
-            statusText: "Resource not found",
-            message: "URL not found",
-          });
-      } else {
-        res.redirect(302, url.originalUrl);
-      }
-    } catch (e) {
-      console.error(e);
-      res.status(500).json({ message: "Server error" });
-    }
-  });
+  app.get("/:shortenedUrlParam", redirectShortenedUrlController);
 }
