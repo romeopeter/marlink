429d2d6e72082f3cdd6809b8b0f9061e455a47fb
romeo <romeomeshack34@gmail.com>
2023-05-10 00:16:36 +0100

    Added routes for getting all and deleting url

--- src/routes.ts
+++ src/routes.ts
@@ -5,14 +5,22 @@ import {
   indexController,
   shortenUrlController,
   redirectShortenedUrlController,
+  allShortenedUrlController,
+  deleteShortenedUrlController
 } from "./controllers/url.controller";
 
 export default function routes(app: Application) {
   app.get("/", indexController);
 
-  // Shorten Url
+  // Shorten URL
   app.post("/marlink/shortenurl", shortenUrlController);
 
   // Redirect to original URl
   app.get("/:shortenedUrlParam", redirectShortenedUrlController);
+
+  // Get all shortened/truncated URLs
+  app.get("/marlink/allurls", allShortenedUrlController)
+
+  // Delete shortened/truncated URL
+  app.get("/marlink/deleteUrl", deleteShortenedUrlController)
 }
